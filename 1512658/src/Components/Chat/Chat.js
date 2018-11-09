eb5e9b55a0c8abc54d1e3a34bcc43d16bbebda52
touyen31 <32916788+touyen31@users.noreply.github.com>
2018-11-10 00:12:41 +0630

    Revert "+ add function upload image"
    This reverts commit b2070de6108b500198b295c589d504b0f832d8b5.

--- 1512658/src/Components/Chat/Chat.js
+++ 1512658/src/Components/Chat/Chat.js
@@ -4,7 +4,6 @@ import  './Chat.css'
 import {compose} from 'redux'
 import {firebaseConnect} from 'react-redux-firebase';
 import ItemChat from "./ItemChat";
-import Uploader from "../Uploader";
 class Chat extends Component {
 
     constructor(props) {
@@ -12,14 +11,11 @@ class Chat extends Component {
         this.state = {
             message: '',
             star: null,
-            picture: false
         }
     }
     handleText = e => {
         this.setState({message: e.target.value})
     }
-
-    handlePicture = e => this.setState({picture: e.target.value})
 
     handleSubmit= () => {
 
@@ -38,12 +34,9 @@ class Chat extends Component {
     setStar = () => this.props.firebase.database().ref(`/presence/${this.props.receive.uid}/star/${this.props.meUid}`).set(true);
     setUnStar = () => this.props.firebase.database().ref(`/presence/${this.props.receive.uid}/star/${this.props.meUid}`).set(false);
 
-    closeUploader = () => this.setState({picture: false})
-
     componentWillReceiveProps(newProps){
         if(newProps.receive && newProps.meUid){
             this.props.firebase.database().ref(`/presence/${newProps.receive.uid}/star/${newProps.meUid}`).on('value', snapshot => {
-                console.log('snapshot: ', snapshot.val())
                 this.setState({star: snapshot.val()})
             })
         }
@@ -59,56 +52,44 @@ class Chat extends Component {
         this.props.firebase.database().ref('messages').push(_message);
     }
     render() {
-        if(this.props.receive){
-            var list = makelist(this.props.messages, this.props.sender.uid, this.props.receive.uid)
-        }
-        console.log('star', this.state.star)
-        return (
-            <div className={'chat'}>
-                {
-                    this.props.receive && <Fragment>
-                        <div className="chat-header clearfix">
-                            <img src={`${this.props.receive.avatarUrl}`}/>
-                            <div className="chat-about">
-                                <div className="chat-with">Chat with {this.props.receive.displayName}</div>
-                            </div>
-                            {
-                                this.state.star ?
-                                    <div>
-                                        <i className="fas fa-star" style={{color: 'yellow'}} onClick={this.setUnStar}></i>
-                                    </div>
-                                    :<div><i className="fas fa-star" onClick={this.setStar}></i></div>
-
-                            }
-
-                        </div>
-                        <div className={'chat-history'}>
-                            <ul>
+            if(this.props.receive){
+                var list = makelist(this.props.messages, this.props.sender.uid, this.props.receive.uid)
+            }
+            return (
+                <div className={'chat'}>
+                    {
+                        this.props.receive && <Fragment>
+                            <div className="chat-header clearfix">
+                                <img src={`${this.props.receive.avatarUrl}`}/>
+                                <div className="chat-about">
+                                    <div className="chat-with">Chat with {this.props.receive.displayName}</div>
+                                </div>
                                 {
-                                    list && list.map(item => <ItemChat data={item} meuid={this.props.meUid}/>)
-                                }
-                            </ul>
-                        </div>
-                        <div className="chat-message clearfix">
-                            {
-                                this.state.picture ? <Uploader sendUrl={this.sendUrl} closeUploader={this.closeUploader}/> :  <textarea name="message-to-send" id="message-to-send" placeholder="Type your message"
-                                                                                                                                        rows="3" onChange={this.handleText} value={this.state.message}></textarea>
-                            }
+                                   this.state.star ?
+                                    <i className="fas fa-star" style={{color: 'yellow'}} onClick={this.setUnStar}></i> : <i className="fas fa-star" onClick={this.setStar}></i>
 
-                            <div onClick={() => this.setState({picture: true})}>
-                                <i className="far fa-file-image"></i>
+                                }
+
+                            </div>
+                            <div className={'chat-history'}>
+                                <ul>
+                                    {
+                                        list && list.map(item => <ItemChat data={item} meuid={this.props.meUid}/>)
+                                    }
+                                </ul>
+                            </div>
+                            <div className="chat-message clearfix">
+                                    <textarea name="message-to-send" id="message-to-send" placeholder="Type your message"
+                                                                                  rows="3" onChange={this.handleText} value={this.state.message}></textarea>
+                                <button onClick={this.handleSubmit}>Send</button>
 
                             </div>
 
-                            <button onClick={this.handleSubmit}>Send</button>
-
-                        </div>
-
-                    </Fragment>
-                }
-            </div>
-        );
-    }
+                        </Fragment>
+                    }
+                </div>
+            );
+        }
 }
 
 const makelist = (messages, sendUid, recvUid) => {
