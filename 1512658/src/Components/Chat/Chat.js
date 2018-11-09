0d9bb6f6326e6501378fb83e59ac118d6cc6ea73
touyen31 <32916788+touyen31@users.noreply.github.com>
2018-11-10 00:15:32 +0630

    + add function upload image

--- 1512658/src/Components/Chat/Chat.js
+++ 1512658/src/Components/Chat/Chat.js
@@ -4,6 +4,7 @@ import  './Chat.css'
 import {compose} from 'redux'
 import {firebaseConnect} from 'react-redux-firebase';
 import ItemChat from "./ItemChat";
+import Uploader from "../Uploader";
 class Chat extends Component {
 
     constructor(props) {
@@ -11,11 +12,14 @@ class Chat extends Component {
         this.state = {
             message: '',
             star: null,
+            picture: false
         }
     }
     handleText = e => {
         this.setState({message: e.target.value})
     }
+
+    handlePicture = e => this.setState({picture: e.target.value})
 
     handleSubmit= () => {
 
@@ -34,9 +38,12 @@ class Chat extends Component {
     setStar = () => this.props.firebase.database().ref(`/presence/${this.props.receive.uid}/star/${this.props.meUid}`).set(true);
     setUnStar = () => this.props.firebase.database().ref(`/presence/${this.props.receive.uid}/star/${this.props.meUid}`).set(false);
 
+    closeUploader = () => this.setState({picture: false})
+
     componentWillReceiveProps(newProps){
         if(newProps.receive && newProps.meUid){
             this.props.firebase.database().ref(`/presence/${newProps.receive.uid}/star/${newProps.meUid}`).on('value', snapshot => {
+                console.log('snapshot: ', snapshot.val())
                 this.setState({star: snapshot.val()})
             })
         }
@@ -52,44 +59,53 @@ class Chat extends Component {
         this.props.firebase.database().ref('messages').push(_message);
     }
     render() {
-            if(this.props.receive){
-                var list = makelist(this.props.messages, this.props.sender.uid, this.props.receive.uid)
-            }
-            return (
-                <div className={'chat'}>
-                    {
-                        this.props.receive && <Fragment>
-                            <div className="chat-header clearfix">
-                                <img src={`${this.props.receive.avatarUrl}`}/>
-                                <div className="chat-about">
-                                    <div className="chat-with">Chat with {this.props.receive.displayName}</div>
-                                </div>
-                                {
-                                   this.state.star ?
+        if(this.props.receive){
+            var list = makelist(this.props.messages, this.props.sender.uid, this.props.receive.uid)
+        }
+        console.log('star', this.state.star)
+        return (
+            <div className={'chat'}>
+                {
+                    this.props.receive && <Fragment>
+                        <div className="chat-header clearfix">
+                            <img src={`${this.props.receive.avatarUrl}`}/>
+                            <div className="chat-about">
+                                <div className="chat-with">Chat with {this.props.receive.displayName}</div>
+                            </div>
+                            {
+                                this.state.star ?
                                     <i className="fas fa-star" style={{color: 'yellow'}} onClick={this.setUnStar}></i> : <i className="fas fa-star" onClick={this.setStar}></i>
 
+                            }
+
+                        </div>
+                        <div className={'chat-history'}>
+                            <ul>
+                                {
+                                    list && list.map(item => <ItemChat data={item} meuid={this.props.meUid}/>)
                                 }
+                            </ul>
+                        </div>
+                        <div className="chat-message clearfix">
+                            {
+                                this.state.picture ? <Uploader sendUrl={this.sendUrl} closeUploader={this.closeUploader}/> :  <textarea name="message-to-send" id="message-to-send" placeholder="Type your message"
+                                                                                                                                        rows="3" onChange={this.handleText} value={this.state.message}></textarea>
+                            }
 
-                            </div>
-                            <div className={'chat-history'}>
-                                <ul>
-                                    {
-                                        list && list.map(item => <ItemChat data={item} meuid={this.props.meUid}/>)
-                                    }
-                                </ul>
-                            </div>
-                            <div className="chat-message clearfix">
-                                    <textarea name="message-to-send" id="message-to-send" placeholder="Type your message"
-                                                                                  rows="3" onChange={this.handleText} value={this.state.message}></textarea>
-                                <button onClick={this.handleSubmit}>Send</button>
+                            <div onClick={() => this.setState({picture: true})}>
+                                <i className="far fa-file-image"></i>
 
                             </div>
 
-                        </Fragment>
-                    }
-                </div>
-            );
-        }
+                            <button onClick={this.handleSubmit}>Send</button>
+
+                        </div>
+
+                    </Fragment>
+                }
+            </div>
+        );
+    }
 }
 
 const makelist = (messages, sendUid, recvUid) => {
---
+++ 1512658/src/Components/Uploader.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { map } from 'lodash';
+import { compose, withHandlers, setPropTypes } from 'recompose';
+import { firebaseConnect } from 'react-redux-firebase';
+import Dropzone from 'react-dropzone';
+
+const filesPath = 'uploadedFiles';
+
+const handlers = {
+    // Uploads files and push's objects containing metadata to database at dbPath
+    onFilesDrop: props => files => {
+        files.map(file => {
+            let upload = props.firebase.storage().ref(`images/${file.name}`).put(file);
+            upload.on('state_changed', function(snapshot){
+
+            }, function(error) {
+            }, function() {
+                upload.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                    props.sendUrl(downloadURL);
+                    props.closeUploader();
+                });
+            });
+        })
+    },
+    onFileDelete: props => (file, key) => {
+        // deleteFile(storagePath, dbPath)
+        return props.firebase.deleteFile(file.fullPath, `${filesPath}/${key}`);
+    }
+};
+
+const enhancerPropsTypes = {
+    firebase: PropTypes.object.isRequired
+};
+
+// Component Enhancer that adds props.firebase and creates a listener for
+// files them passes them into props.uploadedFiles
+const enhance = compose(
+    // Create listeners for Real Time Database which write to redux store
+    firebaseConnect([{ path: filesPath }]),
+    // connect redux state to props
+    connect(({ firebase: { data } }) => ({
+        uploadedFiles: data[filesPath]
+    })),
+    // Set proptypes of props used within handlers
+    setPropTypes(enhancerPropsTypes),
+    // Add handlers as props
+    withHandlers(handlers)
+);
+
+const Uploader = ({ uploadedFiles, onFileDelete, onFilesDrop }) => (
+    <div>
+        <Dropzone onDrop={onFilesDrop}>
+            <div>Drag and drop files here or click to select</div>
+        </Dropzone>
+        {uploadedFiles && (
+            <div>
+                <h3>Uploaded file(s):</h3>
+                {map(uploadedFiles, (file, key) => (
+                    <div key={file.name + key}>
+                        <span>{file.name}</span>
+                        <button onClick={() => onFileDelete(file, key)}>Delete File</button>
+                    </div>
+                ))}
+            </div>
+        )}
+    </div>
+);
+
+Uploader.propTypes = {
+    firebase: PropTypes.object.isRequired,
+    uploadedFiles: PropTypes.object
+};
+
+// Apply enhancer to component on export
+export default enhance(Uploader);
